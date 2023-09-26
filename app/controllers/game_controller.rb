2b267bee8682827760408ed3deb2280965b0b4b5
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-26 16:34:00 +0000

    done

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,14 +2,42 @@ class GameController < ApplicationController
   def rock
 
     @random_move = ["rock","paper","scissors"].sample
-      render({:template => "game_templates/play_rock"})
+    @winner = ""
+    if(@random_move == "rock")
+    @winner = "We tied!"
+    elsif(@random_move == "scissors")
+      @winner = "We won!"
+    else
+      @winner = "We lost!"
+    end
+      render({:template => "game_templates/rock"})
   end
   def paper
-
+    @random_move = ["rock","paper","scissors"].sample
+    @winner = ""
+    if(@random_move == "rock")
+    @winner = "We won!"
+    elsif(@random_move == "scissors")
+      @winner = "We lost!"
+    else
+      @winner = "We tied!"
+    end
+    render({:template => "game_templates/paper"})
   end 
   def scissors
+    @random_move = ["rock","paper","scissors"].sample
+    @winner = ""
+    if(@random_move == "rock")
+    @winner = "We lost!"
+    elsif(@random_move == "scissors")
+      @winner = "We tied!"
+    else
+      @winner = "We won!"
+    end
+    render({:template => "game_templates/scissors"})
   end
   def rules
+    render({:template => "game_templates/rules"})
   end
   
 end
